61ab4844a82077fcb6e6e569cd89e7dfec8a1be2
dwz <1015479445@qq.com>
2016-11-28 15:40:08 +0800

    下载限速

--- web_spider/link_spider2.py
+++ web_spider/link_spider2.py
@@ -4,7 +4,7 @@ import urllib2
 import re
 import urlparse
 import robotparser
-
+from datetime import datetime
 
 def get_links(html):
 	reg = re.compile('<a[^>]+href=["\'](.*?)["\']', re.IGNORECASE)
@@ -28,17 +28,33 @@ def download(url, num_retries=2, proxy=None, user_agent='wswp'):
 				return download(url, num_retries-1, proxy, user_agent)
 	return html
 
-def link_spider(seed_url, link_regex):
+class Throttle:
+	"""docstring for Throttle"""
+	def __init__(self, delay):
+		self.delay = delay
+		self.domains = {}
+
+	def wait(self, url):
+		domain = urlparse.urlparse(url).netloc
+		last_accessed = self.domains.get(domain)
+		if self.delay > 0 and last_accessed is not None:
+			sleep_secs = self.delay - (datetime.now() - last_accessed).seconds
+			if sleep_secs > 0:
+				time.sleep(sleep_secs)
+		self.domains[domain] = datetime.now()
+		
+def link_spider(seed_url, link_regex, delay=0):
 	crawl_queue = [seed_url]
 	seen = set(crawl_queue)
 	rp = robotparser.RobotFileParser()
+	throttle = Throttle(delay)
 	rp.set_url(urlparse.urljoin(seed_url, '/rocot.txt'))
 	rp.read()
 	user_agent = 'GoodCrawler'
-
 	while crawl_queue:
 		url = crawl_queue.pop()
 		if rp.can_fetch(user_agent, seed_url):
+			throttle.wait(url)
 			html = download(url)
 			for link in get_links(html):
 				if re.match(link_regex, link):
