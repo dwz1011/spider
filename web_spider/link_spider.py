b68e619e55125b9b9e473e4567e7c399cb23cb5e
dwz <1015479445@qq.com>
2016-11-28 14:31:20 +0800

    避免重复下载

--- web_spider/link_spider.py
+++ web_spider/link_spider.py
@@ -10,13 +10,16 @@ def get_links(html):
 
 def link_spider(seed_url, link_regex):
 	crawl_queue = [seed_url]
+	seen = set(crawl_queue)
 	while crawl_queue:
 		url = crawl_queue.pop()
 		html = download(url)
 		for link in get_links(html):
 			if re.match(link_regex, link):
 				link = urlparse.urljoin(seed_url,link)
-				crawl_queue.append(link)
+				if link not in seen:
+					seen.add(link)
+					crawl_queue.append(link)
 
 if __name__ == '__main__':
     link_spider('http://example.webscraping.com', '/(index|view)')
