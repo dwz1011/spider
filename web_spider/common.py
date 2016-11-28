d2aaaf228627a9c6ceb4fda99ca9b24350ca5b95
dwz <1015479445@qq.com>
2016-11-28 10:52:51 +0800

    捕获异常

--- web_spider/common.py
+++ web_spider/common.py
@@ -1,7 +1,15 @@
 # -*- coding:utf-8 -*-
 import urllib2
+
 def download(url):
-	return urllib2.urlopen(url).read()
+	print 'downloading:', url
+	try:
+		html = urllib2.urlopen(url).read()
+	except urllib2.URLError as e:
+		print 'Downloading error:', e.reason
+		html = none
+	return html
+
 
 if __name__ == '__main__':
 	url = 'http://example.webscraping.com'
