67efb5ed88bb69f9420d4132fed31a6af396413a
dwz <1015479445@qq.com>
2016-11-28 11:02:55 +0800

    设置用户代理

--- web_spider/common.py
+++ web_spider/common.py
@@ -1,20 +1,22 @@
 # -*- coding:utf-8 -*-
 import urllib2
 
-def download(url, num_retries=2):
+def download(url, num_retries=2, user_agent='wswp'):
 	print 'downloading:', url
+	headers = {'User-agent': user_agent}
+	request = urllib2.Request(url, headers=headers)
 	try:
-		html = urllib2.urlopen(url).read()
+		html = urllib2.urlopen(request).read()
 	except urllib2.URLError as e:
 		print 'Downloading error:', e.reason
 		html = None
 		if num_retries > 0:
 			if hasattr(e, 'code') and 500 <= e.code <= 600:
-				return download(url, num_retries-1)
+				return download(url, num_retries-1, user_agent)
 	return html
 
 
 if __name__ == '__main__':
-	# url = 'http://example.webscraping.com'
-	url = 'http://httpstat.us/500'
+	url = 'http://example.webscraping.com'
+	# url = 'http://httpstat.us/500'
 	print download(url)
