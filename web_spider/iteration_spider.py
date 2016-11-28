ce6a3a66d8fd8f49c33f0a03991c2e0a1195ecaf
dwz <1015479445@qq.com>
2016-11-28 11:36:45 +0800

    ID遍历爬虫，连续发生多次错误后才退出程序

--- web_spider/iteration_spider.py
+++ web_spider/iteration_spider.py
@@ -4,13 +4,18 @@ from common import *
 import itertools
 
 def iteration_spider():
+	max_errors = 5
+	num_errors = 0
 	for page in itertools.count(1):
 		url = 'http://example.webscraping.com/view/- %d' % page
 		html = download(url)
 		if html is None:
-			break
+			num_errors += 1
+			if num_errors == max_errors:
+				break
 		else:
-			pass
+			num_errors = 0
+			
 
 if __name__ == '__main__':
 	iteration_spider()
