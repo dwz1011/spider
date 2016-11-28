ac27b290c7a5e2360643c85fbb38b19b7358db95
dwz <1015479445@qq.com>
2016-11-28 10:58:06 +0800

    函数在发生5xx错误时重新下载

--- web_spider/common.py
+++ web_spider/common.py
@@ -1,16 +1,20 @@
 # -*- coding:utf-8 -*-
 import urllib2
 
-def download(url):
+def download(url, num_retries=2):
 	print 'downloading:', url
 	try:
 		html = urllib2.urlopen(url).read()
 	except urllib2.URLError as e:
 		print 'Downloading error:', e.reason
-		html = none
+		html = None
+		if num_retries > 0:
+			if hasattr(e, 'code') and 500 <= e.code <= 600:
+				return download(url, num_retries-1)
 	return html
 
 
 if __name__ == '__main__':
-	url = 'http://example.webscraping.com'
+	# url = 'http://example.webscraping.com'
+	url = 'http://httpstat.us/500'
 	print download(url)
