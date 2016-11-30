dd54863803377e077c3dd4ef1ac17d024dfbcff2
dwz <1015479445@qq.com>
2016-11-30 11:47:13 +0800

    清理过期数据

--- download_cache/diskcache.py
+++ download_cache/diskcache.py
@@ -3,11 +3,14 @@ import os
 import re
 import urlparse
 import pickle
+from datetime import datetime, timedelta
+import zlib
 
 class DiskCache:
-	def __init__(self, cache_dir='cache'):
+	def __init__(self, cache_dir='cache', expires=timedelta(days=30)):		#timedelta将过期时间设置为30天
 		self.cache_dir = cache_dir
 		self.max_length = max_length
+		self.expires = expires
 	# 文件名限制
 	def url_to_path(self, url):
 		components = urlparse.urlsplit(url)
@@ -26,7 +29,10 @@ class DiskCache:
 		path = self.url_to_path(url)
 		if os.path.exists(path):
 			with open(path, 'rb') as fp:
-				return pickle.load(fp)
+				result, timestamp = pickle.loads(zlib.decompress(fp.read()))
+				if self.has_expired(timestamp):
+					raise KeyError(url + 'has expired')
+				return result
 		else:
 			raise KeyError(url + 'does not exist')
 
@@ -34,10 +40,15 @@ class DiskCache:
 	def __setitem__(self, url, result):
 		path = self.url_to_path(url)
 		folder = os.path.dirname(path)
+		timestamp = datetime.utcnow()
+		data = pickle.dumps((result, timestamp))
 		if not os.path.exists(folder):
 			os.makedirs(folder)
 		with open(path, 'wb') as fp:
-			fp.write(pickle.dumps(result))
+			fp.write(zlib.compress(data))
+
+	def has_expired(self, timestamp):
+		return datetime.utcnow() > timestamp + self.expires
 
 
 
